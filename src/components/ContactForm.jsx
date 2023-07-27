0e8451a94512d2c91907d5d37b5b7395ae04dcb0
Adam Herro <adam.herro@comcast.net>
2023-07-27 11:58:48 -0400

    Adding functionality to send contact form info to console, working on design

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,21 +1,61 @@
-const ContactForm = () => {
-  return (
-    <form className="flex flex-col items-center border border-black mx-96 py-10">
-        <div className="flex flex-col items-start">
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" className="w-[320px] bg-white border border-black" />
-        </div>
-        <div className="flex flex-col items-start">
-            <label htmlFor="name">Subject</label>
-            <input type="text" name="name" id="name" className="w-[320px] bg-white border border-black" />
-        </div>
-        <div className="flex flex-col items-start">
-            <label htmlFor="name">Message</label>
-            <textarea type="text" name="name" id="name" className="textarea w-[1000px] max-w-xs resize bg-white border border-black" />
-        </div>
+import { useEffect, useState } from "react"
 
-    </form>
-  )
+const ContactForm = () => {
+    const [message, setMessage] = useState({
+        name: '',
+        subject: '',
+        text: ''
+    })
+
+    const writeName = (e) => {
+        setMessage({
+            ...message,
+            name: e.target.value
+        })
+    }
+    const writeSubject = (e) => {
+        setMessage({
+            ...message,
+            subject: e.target.value
+        })
+    }
+    const writeText = (e) => {
+        setMessage({
+            ...message,
+            text: e.target.value
+        })
+    }
+
+    const submitForm = (e) => {
+        e.preventDefault()
+        console.log(message)
+        setMessage({
+            name: '',
+            subject: '',
+            text: ''
+        })
+
+    }
+
+    return (
+        <form className="flex w-full justify-center pb-10" onSubmit={submitForm}>
+            <div className="flex flex-col items-center border border-black">
+                <div className="flex flex-col items-start mx-4 my-2">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" id="name" className="w-[320px] bg-white border border-black" value={message.name} onChange={writeName} />
+                </div>
+                <div className="flex flex-col items-start mx-4 my-2">
+                    <label htmlFor="subject">Subject</label>
+                    <input type="text" name="subject" id="subject" className="w-[320px] bg-white border border-black" value={message.subject} onChange={writeSubject} />
+                </div>
+                <div className="flex flex-col items-start mx-4 my-2">
+                    <label htmlFor="message">Message</label>
+                    <textarea type="text" name="message" id="message" className="textarea w-[1000px] max-w-xs resize bg-white border border-black" onChange={writeText} value={message.text}/>
+                </div>
+                <input type="submit" value="Send Message" className="btn btn-primary my-4 w-[320px]" />
+            </div>
+        </form>
+    )
 }
 
 export default ContactForm
--- src/components/Custom.jsx
+++ src/components/Custom.jsx
@@ -6,7 +6,7 @@ const Custom = () => {
         <h3 className="text-center font-bold text-3xl mt-10">Need something customized?</h3>
         <p className="text-center font-bold text-lg">Contact us to discuss your vision so we can bring it to life.</p>
         <Link to='/contact'>
-            <button className="btn btn-primary mt-4">Contact Us</button>
+            <button className="btn btn-primary mt-4 mb-12">Contact Us</button>
         </Link>
     </section>
   )
