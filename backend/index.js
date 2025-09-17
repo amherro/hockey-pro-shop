3c08221d8955c8dccb4fda989d94dae460c7643d
Adam Herro <adam.herro@comcast.net>
2025-09-17 12:07:51 -0400

    Rerouting to Thank You page after successful purchase.

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,48 @@
-const stripe = require("stripe")(process.env.STRIPE_API_KEY)
-const express = require("express")
+const stripe = require('stripe')(process.env.STRIPE_API_KEY);
+const express = require('express');
 const cors = require('cors');
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-const app = express()
-app.use(cors())
-app.use(express.static('public'))
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
 
 app.post('/checkout', async (req, res) => {
-    try {
-        const itemsInCart = req.body.items
-        
-        console.log(itemsInCart)
-        
-        let lineItems = []
-    
-        itemsInCart.forEach(item => {
-            lineItems.push({price: item.id, quantity: item.quantity})
-        })
-        
-        const session = await stripe.checkout.sessions.create({
-            line_items: lineItems,
-            mode: 'payment',
-            success_url: `${process.env.NODE_ENV === 'production' ? 'https://hockey-ecommerce-store.onrender.com/?success=true' : 'http://localhost:5001/?success=true'}`,
-            cancel_url: `${process.env.NODE_ENV === 'production' ? 'https://hockey-ecommerce-store.onrender.com/?canceled=true' : 'http://localhost:5001/?canceled=true'}`,
-        })
-        console.log(session.url)
-        res.send(JSON.stringify({
-            url: session.url
-        }));
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const itemsInCart = req.body.items;
 
-})
+    console.log(itemsInCart);
+
+    let lineItems = [];
+
+    itemsInCart.forEach((item) => {
+      lineItems.push({ price: item.id, quantity: item.quantity });
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: `${
+        process.env.NODE_ENV === 'production'
+          ? 'https://hockey-ecommerce-store.onrender.com/thankyou?success=true'
+          : 'http://localhost:5001/thankyou?success=true'
+      }`,
+      cancel_url: `${
+        process.env.NODE_ENV === 'production'
+          ? 'https://hockey-ecommerce-store.onrender.com/?canceled=true'
+          : 'http://localhost:5001/?canceled=true'
+      }`,
+    });
+    console.log(session.url);
+    res.send(
+      JSON.stringify({
+        url: session.url,
+      })
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
--- frontend/src/pages/ThankYou.jsx
+++ frontend/src/pages/ThankYou.jsx
@@ -1,21 +1,28 @@
 // import { Link } from 'react-router-dom'
-import Header from '../components/Header'
+import Header from '../components/Header';
 
 const ThankYou = () => {
   return (
     <>
       <Header />
-      <section className='py-20 pb-12 mx-5'>
-        <h3 className='text-center font-bold text-2xl pb-4'>Thank you for your order!</h3>
-        <h4 className='text-center font-bold text-xl'>We&apos;ll begin fulfilling it and email you when it&apos;s on the way.</h4>
+      <section className="py-20 pb-12 mx-5">
+        <h3 className="text-center font-bold text-2xl pb-4">
+          Thank you for your order!
+        </h3>
+        <h4 className="text-center font-bold text-xl">
+          We&apos;ll begin fulfilling it and email you when it&apos;s on the
+          way.
+        </h4>
       </section>
-      <section className='flex justify-center pb-10'>
-        <button className='btn bg-blue-700 text-white mr-2 border-none'>
-          <a href='/'>Continue Shopping</a>
-        </button>
+      <section className="flex justify-center pb-10">
+        <a href="/">
+          <button className="btn bg-blue-700 text-white mr-2 border-none">
+            Continue Shopping
+          </button>
+        </a>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default ThankYou
+export default ThankYou;
